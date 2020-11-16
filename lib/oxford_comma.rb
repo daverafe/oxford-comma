a71a3d6569ef290ee759e3439df2511617d5871e
David Rafe <david.rafe@gmail.com>
2020-11-15 22:45:01 -0500

    commit tree 15712a4e59843aee5922520504f2564dbe2b188a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,30 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#     case array
+#     when array.length < 2
+#         array.join(", ")
+#     when array.length == 2
+#         array.join(" and ")
+#     when array.length == 3
+#         array.join(", and ")
+#     when array.length > 3
+#         array.join(", and ")
+#     else
+#     end
+# end
 
+
+def oxford_comma(array)
+    if array.length < 2
+        array.join(", ")
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length == 3
+        last = array.pop
+        array << "and " + last 
+        array.join(", ")
+    elsif array.length > 3
+        last = array.pop 
+        array << "and " + last 
+        array.join(", ")
+    end
 end
